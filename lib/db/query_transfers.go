fa23219f7efa4fe01627c8c19c1db28ee476fa79
Jaeiya <exanix@gmail.com>
2024-09-13 09:00:47 -0700

    upd(sqlite): return all transfer results

--- lib/db/query_transfers.go
+++ lib/db/query_transfers.go
@@ -1,6 +1,10 @@
 package db
 
-import "github.com/jaeiya/billbank/lib"
+import (
+	"fmt"
+
+	"github.com/jaeiya/billbank/lib"
+)
 
 type TransferType string
 
@@ -54,63 +58,65 @@ func (sdb SqliteDb) CreateTransfer(td TransferConfig) error {
 	return nil
 }
 
-func (sdb SqliteDb) QueryTransfer(qm QueryMap) TransferData {
-	return sdb.queryTransfers(buildFieldMap(BY_ID|BY_MONTH_ID|BY_BANK_ACCOUNT_ID, qm))
-}
-
-func (sdb SqliteDb) queryTransfers(wm FieldMap) TransferData {
-	queryStr := createQueryStr(TRANSFERS, wm)
+func (sdb SqliteDb) QueryTransfers(qm QueryMap) ([]TransferData, error) {
+	fieldMap := buildFieldMap(BY_ID|BY_MONTH_ID|BY_BANK_ACCOUNT_ID, qm)
+	queryStr := createQueryStr(TRANSFERS, fieldMap)
 	rows, err := sdb.handle.Query(queryStr)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 
-	if !rows.Next() {
-		panic("transfers table is empty")
-	}
-
 	var (
-		id        int
-		accountID int
-		monthID   int
-		name      string
-		amount    int
-		date      string
-		tt        TransferType
-		toWhom    *string
-		fromWhom  *string
+		id           int
+		accountID    int
+		monthID      int
+		name         string
+		amount       int
+		date         string
+		tt           TransferType
+		toWhom       *string
+		fromWhom     *string
+		transferRows []TransferData
 	)
 
-	err = rows.Scan(
-		&id,
-		&accountID,
-		&monthID,
-		&name,
-		&amount,
-		&date,
-		&tt,
-		&toWhom,
-		&fromWhom,
-	)
-	if err != nil {
-		panic(err)
-	}
-	c := lib.NewCurrency("", sdb.currencyCode)
-	c.LoadAmount(amount)
+	for rows.Next() {
+		err = rows.Scan(
+			&id,
+			&accountID,
+			&monthID,
+			&name,
+			&amount,
+			&date,
+			&tt,
+			&toWhom,
+			&fromWhom,
+		)
+		if err != nil {
+			panic(err)
+		}
+		c := lib.NewCurrency("", sdb.currencyCode)
+		c.LoadAmount(amount)
 
-	d := TransferData{
-		ID: id,
-		TransferConfig: TransferConfig{
-			AccountID:    accountID,
-			MonthID:      monthID,
-			Name:         name,
-			Amount:       c,
-			Date:         date,
-			TransferType: tt,
-			ToWhom:       toWhom,
-			FromWhom:     fromWhom,
-		},
+		transferRows = append(transferRows, TransferData{
+			ID: id,
+			TransferConfig: TransferConfig{
+				AccountID:    accountID,
+				MonthID:      monthID,
+				Name:         name,
+				Amount:       c,
+				Date:         date,
+				TransferType: tt,
+				ToWhom:       toWhom,
+				FromWhom:     fromWhom,
+			},
+		})
+
 	}
-	return d
+
+	if len(transferRows) == 0 {
+		return transferRows, fmt.Errorf("no data found")
+	}
+
+	return transferRows, nil
 }
