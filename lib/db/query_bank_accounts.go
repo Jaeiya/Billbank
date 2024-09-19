1f30ede5687d2601ea07898fafc55b2f04a4f71c
Jaeiya <exanix@gmail.com>
2024-09-19 12:14:26 -0700

    upd(sqlite): move transfers into bank account query file
    Transfers are directly related to a specific bank account
    so it doesn't really make sense for them to be in their
    own file.

--- lib/db/query_bank_accounts.go
+++ lib/db/query_bank_accounts.go
@@ -6,6 +6,14 @@ import (
 	"github.com/jaeiya/billbank/lib"
 )
 
+type TransferType string
+
+const (
+	WITHDRAWAL = TransferType("withdrawal")
+	DEPOSIT    = TransferType("deposit")
+	MOVE       = TransferType("move")
+)
+
 type BankAccountConfig struct {
 	Name          string
 	Password      *string
@@ -51,6 +59,23 @@ type BankHistoryConfig struct {
 	MonthID       int
 	BankAccountID int
 	Balance       lib.Currency
+}
+
+type TransferConfig struct {
+	BankAccountID int
+	MonthID       int
+	Name          string
+	Amount        lib.Currency
+	Date          string
+	TransferType  TransferType
+
+	ToWhom   *string
+	FromWhom *string
+}
+
+type TransferRecord struct {
+	TransferConfig
+	ID int
 }
 
 func (sdb SqliteDb) CreateBankAccount(config BankAccountConfig) {
@@ -172,6 +197,55 @@ func (sdb SqliteDb) QueryBankAccountHistory(qm QueryMap) ([]BankHistoryRecord, e
 
 	if len(records) == 0 {
 		return []BankHistoryRecord{}, fmt.Errorf("no query results found")
+	}
+
+	return records, nil
+}
+
+func (sdb SqliteDb) CreateTransfer(td TransferConfig) {
+	if _, err := sdb.handle.Exec(
+		sdb.InsertInto(
+			TRANSFERS,
+			td.BankAccountID,
+			td.MonthID,
+			td.Name,
+			td.Amount.GetStoredValue(),
+			td.Date,
+			td.TransferType,
+			lib.TryDeref(td.ToWhom),
+			lib.TryDeref(td.FromWhom),
+		),
+	); err != nil {
+		panic(err)
+	}
+}
+
+func (sdb SqliteDb) QueryTransfers(qm QueryMap) ([]TransferRecord, error) {
+	rows := sdb.query(TRANSFERS, qm)
+	var amount int
+	var records []TransferRecord
+
+	for rows.Next() {
+		var record TransferRecord
+		if err := rows.Scan(
+			&record.ID,
+			&record.BankAccountID,
+			&record.MonthID,
+			&record.Name,
+			&amount,
+			&record.Date,
+			&record.TransferType,
+			&record.ToWhom,
+			&record.FromWhom,
+		); err != nil {
+			panic(err)
+		}
+		record.Amount = lib.NewCurrencyFromStore(amount, sdb.currencyCode)
+		records = append(records, record)
+	}
+
+	if len(records) == 0 {
+		return records, fmt.Errorf("no data found")
 	}
 
 	return records, nil
