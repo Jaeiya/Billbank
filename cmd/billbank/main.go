e05bc02244a46a428082f25938fd923232da5358
Jaeiya <exanix@gmail.com>
2025-01-29 20:38:45 -0800

    chore(main): remove deprecated commented code

--- cmd/billbank/main.go
+++ cmd/billbank/main.go
@@ -8,11 +8,6 @@ import (
 )
 
 func main() {
-	// wd, err := os.Getwd()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
 	// filePath := filepath.Join(wd, "hello.db")
 	// defer func() {
 	// 	err := os.Remove(filePath)
@@ -22,7 +17,7 @@ func main() {
 	// }()
 	// db := sqlite.NewSqliteDb(filePath, lib.USD)
 	// defer db.Close()
-	// ti := components.NewCommanderInput()
+
 	utils.CreateLog()
 	utils.Log(utils.Info, "Starting BillBank")
 
@@ -39,18 +34,4 @@ func main() {
 	if _, err := p1.Run(); err != nil {
 		panic(err)
 	}
-
-	// p := tea.NewProgram(
-	// 	ui.NewCmdInput(
-	// 		ui.WithCommands(commands.NewTestCmd()),
-	// 	),
-	// 	tea.WithAltScreen(),
-	// )
-	// if _, err := p.Run(); err != nil {
-	// 	panic(err)
-	// }
-
-	// cmd := commands.NewTestCmd()
-	// res := cmd.ParseCommand("view bills amount")
-	// fmt.Printf("%v\n", res)
 }
