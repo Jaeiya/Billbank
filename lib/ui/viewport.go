d0f3ddb9145c6a2b597da773ec59f1615802dc86
Jaeiya <exanix@gmail.com>
2025-02-03 16:14:26 -0800

    feat(viewport): log terminal size

--- lib/ui/viewport.go
+++ lib/ui/viewport.go
@@ -43,7 +43,7 @@ func (vp ViewPort) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		vp.height = msg.Height
 		vp.width = msg.Width
-		utils.Log(utils.Info, fmt.Sprintf("ViewPortSize: %dx%d", vp.width, vp.height))
+		utils.Log(utils.Debug, fmt.Sprintf("ViewPortSize: %dx%d", vp.width, vp.height))
 		cmds = append(cmds, vp.sendViewportSize)
 
 	case CommandModel:
