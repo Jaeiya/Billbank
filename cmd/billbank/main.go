7469d084169fb2b155bcbf9aa0bc3f09fda9d20c
Jaeiya <exanix@gmail.com>
2025-02-16 20:21:54 -0800

    chore(main): move db code out of main func
    I need to work in there...

--- cmd/billbank/main.go
+++ cmd/billbank/main.go
@@ -9,16 +9,17 @@ import (
 	"github.com/jaeiya/billbank/lib/utils"
 )
 
+// filePath := filepath.Join(wd, "hello.db")
+// defer func() {
+// 	err := os.Remove(filePath)
+// 	if err != nil {
+// 		panic(err)
+
+// }()
+// db := sqlite.NewSqliteDb(filePath, lib.USD)
+// defer db.Close()
+
 func main() {
-	// filePath := filepath.Join(wd, "hello.db")
-	// defer func() {
-	// 	err := os.Remove(filePath)
-	// 	if err != nil {
-	// 		panic(err)
-	//
-	// }()
-	// db := sqlite.NewSqliteDb(filePath, lib.USD)
-	// defer db.Close()
 
 	logger.SetLogLevel(logger.Info)
 	logger.Log(logger.Info, "Starting BillBank")
