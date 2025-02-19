9a2178f16c6c9a7931eb949cffcb728387bc8c70
Jaeiya <exanix@gmail.com>
2025-02-18 17:15:17 -0800

    refactor(cmdskeleton): update to reflect latest changes to commands
    This also adds all the documentation needed to understand how to create
    a new command.

--- lib/commands/_cmd_skeleton.go
+++ lib/commands/_cmd_skeleton.go
@@ -2,46 +2,87 @@ import (
 	"fmt"
 
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/jaeiya/billbank/lib/cmd"
+	"github.com/jaeiya/billbank/lib/cmdmodel"
 )
 
-func NewSkeletonCmd() cmd.Command {
-	//
-	// Anything that needs to be initialized should be here
-	// so that it can be passed to your model.
-	//
-	m := skeletonModel{}
-
-	return cmd.New(
-		cmd.Config{
-			Model: m,
-		},
-	)
-}
-
+// Main model that contains all data for your new command.
+// (rename to reflect the name of your new command)
 type skeletonModel struct {
-	*cmd.BaseModel[skeletonModel]
+	*cmdmodel.ModelBase[skeletonModel]
 	thisCounter int
 	thatCounter int
 }
 
-func (m skeletonModel) Update(msg tea.Msg) (cmd.Model, tea.Cmd) {
+// Shorten type for convenience
+// (rename it to reflect the command name)
+type skeletonCmd = cmdmodel.BaseCommand[skeletonModel]
+
+// Creates a new command using the base model
+// (rename it to reflect the command name)
+func NewSkeletonCmd() cmdmodel.Model {
+	//
+	// Do NOT put initialization code here
+	//
+	return cmdmodel.New(
+		newSkeletonModel(
+			"Skeleton",
+			[]string{"test"},
+			[]skeletonCmd{
+				{Path: "", Run: loadDefaultCmd, View: loadDefaultView},
+				{Path: "this", Run: loadThis, View: thisView},
+				{Path: "that", Run: loadThat, View: thatView},
+			}),
+	)
+}
+
+// newSkeletonModel demonstrates how to initialize a new
+// command model. All initialization code for your model
+// should go in here.
+// (rename it to reflect the command name)
+func newSkeletonModel(name string, aliases []string, commands []skeletonCmd) skeletonModel {
+	return skeletonModel{
+		ModelBase: cmdmodel.NewModelBase(cmdmodel.BaseCmdData[skeletonModel]{
+			Name:     name,
+			Aliases:  aliases,
+			Commands: commands,
+		}),
+	}
+}
+
+func (m skeletonModel) Update(msg tea.Msg) (cmdmodel.Interface, tea.Cmd) {
 	var teaCmd tea.Cmd
 	var teaCmds []tea.Cmd
 
-	m, teaCmd = m.BaseModel.Update(m, msg)
+	//- DO NOT REMOVE; required for base model interaction
+	m, teaCmd = m.ModelBase.Update(m, msg)
 	teaCmds = append(teaCmds, teaCmd)
+	//--------------------------------------------------//
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-
+		if msg.String() == "ctrl+k" {
+			m.thisCounter += 1
+		}
 	}
 
 	return m, tea.Batch(teaCmds...)
 }
 
 func (m skeletonModel) View() string {
-	return m.BaseModel.View(m)
+	// DO NOT REMOVE; required for base model interaction
+	return m.ModelBase.View(m)
+}
+
+//##########################################
+//     Custom Functions Go Below Here
+//##########################################
+
+func loadDefaultCmd(m skeletonModel) skeletonModel {
+	return m
+}
+
+func loadDefaultView(m skeletonModel) string {
+	return "default view"
 }
 
 func loadThis(m skeletonModel) skeletonModel {
@@ -50,7 +91,7 @@ func loadThis(m skeletonModel) skeletonModel {
 
 func thisView(m skeletonModel) string {
 	return fmt.Sprintf(
-		"Hit ctrl+k to increment the counter: %d",
+		"Hit ctrl+k to increment this counter: %d",
 		m.thisCounter,
 	)
 }
@@ -61,5 +102,12 @@ func loadThat(m skeletonModel) skeletonModel {
 }
 
 func thatView(m skeletonModel) string {
-	return fmt.Sprintf("Execute the command again to increment the counter: %d", m.thatCounter)
+	text := `
+Execute the command again to increment that counter: %d
+
+If you resize the window, it will also increment,
+this is because layout recalculations should happen
+when the window is resized.
+`
+	return fmt.Sprintf(text, m.thatCounter)
 }
