50a631af7804a7cfc71c1df23b58832a0b8493f0
Jaeiya <exanix@gmail.com>
2025-02-18 17:24:53 -0800

    refactor(cmdbills): use new template for creating commands

--- lib/commands/bills.go
+++ lib/commands/bills.go
@@ -5,57 +5,61 @@ import (
 	"github.com/jaeiya/billbank/lib/cmdmodel"
 )
 
-func NewBillsCmd() cmdmodel.Model {
-	//
-	// Anything that needs to be initialized should be here
-	// so that it can be passed to your model.
-	//
-	m := billsModel{
-		ModelBase: cmdmodel.NewModelBase(cmdmodel.BaseCmdData[billsModel]{
-			Name:    "Bills",
-			Aliases: []string{"bills", "b"},
-			Commands: []cmdmodel.BaseCommand[billsModel]{
-				{Path: "", Run: loadBills, View: viewBills},
-			},
-		}),
-	}
-
-	return cmdmodel.New(m)
-}
-
 type billsModel struct {
 	*cmdmodel.ModelBase[billsModel]
+}
+
+type billCmd = cmdmodel.BaseCommand[billsModel]
+
+func NewBillsCmd() cmdmodel.Model {
+	return cmdmodel.New(
+		newBillsModel(
+			"Bills",
+			[]string{"bills"},
+			[]billCmd{
+				{
+					Path: "",
+					Run:  func(bm billsModel) billsModel { return bm },
+					View: func(bm billsModel) string { return "bills command" },
+				},
+			}),
+	)
+}
+
+func newBillsModel(name string, aliases []string, commands []billCmd) billsModel {
+	return billsModel{
+		ModelBase: cmdmodel.NewModelBase(cmdmodel.BaseCmdData[billsModel]{
+			Name:     name,
+			Aliases:  aliases,
+			Commands: commands,
+		}),
+	}
 }
 
 func (m billsModel) Update(msg tea.Msg) (cmdmodel.Interface, tea.Cmd) {
 	var teaCmd tea.Cmd
 	var teaCmds []tea.Cmd
 
+	//- DO NOT REMOVE; required for base model interaction
 	m, teaCmd = m.ModelBase.Update(m, msg)
 	teaCmds = append(teaCmds, teaCmd)
+	//--------------------------------------------------//
 
 	// switch msg := msg.(type) {
 	// case tea.KeyMsg:
-
-	// 	// Only watch certain keys on certain cmd paths
-	// 	// if m.IsActivePath("t this") || m.IsActivePath("test this") {
-	// 	// 	if msg.String() == "ctrl+k" {
-	// 	// 		m.thisCounter += 1
-	// 	// 	}
-	// 	// }
+	// 	if msg.String() == "ctrl+k" {
+	// 		m.thisCounter += 1
+	// 	}
 	// }
 
 	return m, tea.Batch(teaCmds...)
 }
 
 func (m billsModel) View() string {
+	// DO NOT REMOVE; required for base model interaction
 	return m.ModelBase.View(m)
 }
 
-func loadBills(m billsModel) billsModel {
-	return m
-}
-
-func viewBills(m billsModel) string {
-	return "Hello, " + m.GetCmdArg()
-}
+//##########################################
+//     Custom Functions Go Below Here
+//##########################################
