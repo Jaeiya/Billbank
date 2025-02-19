e0668a907707a54110ccba19659ddef2c97101ed
Jaeiya <exanix@gmail.com>
2025-02-18 17:22:14 -0800

    docs(cmdskeleton): clarify base model fields

--- lib/commands/_cmd_skeleton.go
+++ lib/commands/_cmd_skeleton.go
@@ -25,11 +25,17 @@ func NewSkeletonCmd() cmdmodel.Model {
 	//
 	return cmdmodel.New(
 		newSkeletonModel(
+			// Command Name
 			"Skeleton",
+			// Aliases
 			[]string{"test"},
+			// Commands
 			[]skeletonCmd{
+				// Default command (cmd executed by itself)
 				{Path: "", Run: loadDefaultCmd, View: loadDefaultView},
+				// Creates "test this" command
 				{Path: "this", Run: loadThis, View: thisView},
+				// Creates "test that" command
 				{Path: "that", Run: loadThat, View: thatView},
 			}),
 	)
