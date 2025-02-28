44cf5794674920fa6b47f110f45a0854bd144ee4
Jaeiya <exanix@gmail.com>
2025-02-28 09:21:11 -0800

    feat(main): load database and add logs

--- cmd/billbank/main.go
+++ cmd/billbank/main.go
@@ -1,30 +1,41 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/jaeiya/billbank/lib"
 	"github.com/jaeiya/billbank/lib/cmdmodel"
 	"github.com/jaeiya/billbank/lib/commands"
+	"github.com/jaeiya/billbank/lib/db/sqlite"
 	"github.com/jaeiya/billbank/lib/logger"
 	"github.com/jaeiya/billbank/lib/utils"
 )
 
-// filePath := filepath.Join(wd, "hello.db")
-// defer func() {
-// 	err := os.Remove(filePath)
-// 	if err != nil {
-// 		panic(err)
-
-// }()
-// db := sqlite.NewSqliteDb(filePath, lib.USD)
-// defer db.Close()
-
 func main() {
-	err := logger.SetLogLevel(logger.Info)
+	err := logger.SetLogLevel(logger.Debug)
 	if err != nil {
 		panic(err)
 	}
-	logger.Log(logger.Info, "Starting BillBank")
+	logger.Log(logger.Info, "starting billbank")
+
+	filePath := filepath.Join(utils.GetWorkingDir(), "billbank.db")
+	// TODO  Do not delete database in production
+	defer func() {
+		err := os.Remove(filePath)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	db, err := sqlite.NewSqliteDb(filePath, lib.USD)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	logger.Log(logger.Info, "loaded database")
 
 	vp := lib.ViewPort{}
 	h := utils.NewCmdHistory()
@@ -35,6 +46,8 @@ func main() {
 		commands.NewBillsCmd(),
 		commands.NewDebugCmd(h),
 	)
+
+	logger.Log(logger.Info, "finished loading commands")
 
 	p1 := tea.NewProgram(
 		vp,
