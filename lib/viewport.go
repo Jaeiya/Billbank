2c4efc530a185ead424dc8c1329571c9d1e00e52
Jaeiya <exanix@gmail.com>
2025-02-28 10:13:47 -0800

    fix(viewport): do not return out of window size msg

--- lib/viewport.go
+++ lib/viewport.go
@@ -49,7 +49,7 @@ func (vp ViewPort) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			vp.height = msg.Height
 			vp.width = msg.Width
 			logger.Log(logger.Hot, "[WindowSizeMsg] sending viewport size [%d:%d]", vp.width, vp.height)
-			return vp, vp.sendViewportSize
+			teaCmds = append(teaCmds, vp.sendViewportSize)
 		}
 
 	case tea.KeyMsg:
