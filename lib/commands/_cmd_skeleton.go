582f678501c3f76a87a75f988018929e65363c7b
Jaeiya <exanix@gmail.com>
2025-02-28 14:26:41 -0800

    refactor(cmdskeleton): update with latest paradigms

--- lib/commands/_cmd_skeleton.go
+++ lib/commands/_cmd_skeleton.go
@@ -1,8 +1,11 @@
+
 import (
 	"fmt"
+	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/jaeiya/billbank/lib/cmdmodel"
+	"github.com/jaeiya/billbank/lib/utils"
 )
 
 // Main model that contains all data for your new command.
@@ -11,6 +14,8 @@ type skeletonModel struct {
 	*cmdmodel.Base[skeletonModel]
 	thisCounter int
 	thatCounter int
+	lastKey     string
+	cInt        int
 }
 
 // Shortened type for convenience
@@ -20,12 +25,48 @@ type skeletonCmd = cmdmodel.Command[skeletonModel]
 // Hook up all the commands to the command model
 // (rename it to reflect the command name)
 var skeletonCommands = []skeletonCmd{
-	// Default command (cmd executed by itself)
-	{Path: "", Run: loadDefaultCmd, View: loadDefaultView},
-	// Creates "test this" command
-	{Path: "this", Run: loadThis, View: thisView},
-	// Creates "test that" command
-	{Path: "that", Run: loadThat, View: thatView},
+	// Creates "test" command which demonstrates how to create
+	// what's called a 'default command' where the alias
+	// can be executed by itself. If you add the arguments
+	// flag, then it can receive arguments.
+	{Path: "", Run: loadDummy, View: loadDefaultView},
+
+	// Creates a "test a" command which increments a
+	// counter when ctrl+k is pressed.
+	{Path: "a", Run: loadDummy, View: viewA},
+
+	// Creates a "test b" command which increments a
+	// counter every time the command is executed.
+	{Path: "b", Run: loadB, View: viewB},
+
+	// Creates "test c" command which demonstrates how
+	// to parse a required argument and receive it properly
+	// with error handling. Optional arguments also require
+	// a parse method.
+	{
+		Path:    "c",
+		Run:     loadC,
+		View:    viewC,
+		ArgType: cmdmodel.ArgRequired,
+		// All arguments require this func, which converts
+		// the user input into a valid value. If there's
+		// an error, it should return a user-readable
+		// message.
+		ParseArg: func(arg string) (any, error) {
+			i, err := utils.ParseInt(arg)
+			if err != nil {
+				if strings.Contains(err.Error(), "out of range") {
+					return nil, fmt.Errorf("number is too large")
+				}
+				return nil, fmt.Errorf("'%s' is not an integer", arg)
+			}
+			return i, nil
+		},
+	},
+
+	// Creates a "test d" command which demonstrates how
+	// the CaptureInput flag works.
+	{Path: "d", Run: loadDummy, View: viewD, CaptureInput: true},
 }
 
 // newSkeletonModel demonstrates how to initialize a new
@@ -61,6 +102,8 @@ func (m skeletonModel) Update(msg tea.Msg) (cmdmodel.Interface, tea.Cmd) {
 		if msg.String() == "ctrl+k" {
 			m.thisCounter += 1
 		}
+
+		m.lastKey = msg.String()
 	}
 
 	return m, tea.Batch(teaCmds...)
@@ -71,11 +114,12 @@ func (m skeletonModel) View() string {
 	return m.Base.View(m)
 }
 
-//##########################################
-//     Custom Functions Go Below Here
-//##########################################
-
-func loadDefaultCmd(m skeletonModel) skeletonModel {
+// ##########################################
+//
+//	Custom Functions Go Below Here
+//
+// ##########################################
+func loadDummy(m skeletonModel) skeletonModel {
 	return m
 }
 
@@ -83,29 +127,85 @@ func loadDefaultView(m skeletonModel) string {
 	return "default view"
 }
 
-func loadThis(m skeletonModel) skeletonModel {
-	return m
-}
-
-func thisView(m skeletonModel) string {
+func viewA(m skeletonModel) string {
 	return fmt.Sprintf(
-		"Hit ctrl+k to increment this counter: %d",
+		`Hit ctrl+k to increment this counter: %d
+
+Did it not work? That's because you'll need to hit the
+Grave key (key below Esc key) to toggle the input. By
+default, the command box takes input precedence.
+
+Commands that need input from the user on load, can
+use the CaptureInput flag, which will hide the
+command box on command execution.
+`,
 		m.thisCounter,
 	)
 }
 
-func loadThat(m skeletonModel) skeletonModel {
+func loadB(m skeletonModel) skeletonModel {
 	m.thatCounter += 1
 	return m
 }
 
-func thatView(m skeletonModel) string {
+func viewB(m skeletonModel) string {
 	text := `
-Execute the command again to increment that counter: %d
+Execute the command again to increment the counter: %d
 
-If you resize the window, it will also increment,
-this is because layout recalculations should happen
-when the window is resized.
+When resizing the window, you will not see the
+counter update, even though technically the
+view is re-rendered. This is because window
+resizes do not re-execute command logic.
+
+This means that you'll need to place all
+layout logic into the view. The reason for
+this "feature" is because it prevents
+expensive commands from lagging the UI
+during window resizing.
 `
 	return fmt.Sprintf(text, m.thatCounter)
+}
+
+func loadC(m skeletonModel) skeletonModel {
+	// This func is very important because it will propagate
+	// configuration errors if it finds any. This is the
+	// only reliable way to retrieve command arguments.
+	myInt, isValid := cmdmodel.GetArgAs[int](m.Base)
+	if !isValid {
+		return m
+	}
+
+	m.cInt = myInt // do something with the arg
+	return m
+}
+
+func viewC(m skeletonModel) string {
+	return fmt.Sprintf(`
+You entered the number: %d
+
+Validating arguments is very important and the error
+messages returned to the user should always clue them
+in on what they did wrong.
+`, m.cInt)
+}
+
+func viewD(m skeletonModel) string {
+	return fmt.Sprintf(`
+Last key entered: %s
+
+This view captures the input immediately
+without you having to hit the grave key first.
+
+Try typing some keys and see what happens!
+
+The grave key (key under Esc key) toggles the
+command input box at the bottom of the screen.
+When it's hidden, all key input is transferred
+to the view.
+
+This means if the command input is active, you
+won't see the keys change above, because the
+command input will be intercepting all of
+them.
+`, m.lastKey)
 }
