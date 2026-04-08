a4a1fda14ab111ec34af6d883af83e2f630976a0
Jaeiya <exanix@gmail.com>
2026-04-08 15:45:56 -0700

    chore: commit WIP main entry point
    Only committing this to get ready for project restructure

--- cmd/billbank/main.go
+++ cmd/billbank/main.go
@@ -43,7 +43,7 @@ func main() {
 	vp.CommandInput = cmdmodel.NewInputModel(
 		h,
 		"bills",
-		commands.NewBillsCmd(),
+		commands.NewBillsCmd(db),
 		commands.NewDebugCmd(h),
 	)
 
@@ -64,3 +64,95 @@ func main() {
 		panic(err)
 	}
 }
+
+// db, err := sqlite.NewSqliteDb("billbank.db", lib.USD)
+// if err != nil {
+// 	panic(err)
+// }
+
+// defer func() {
+// 	err := db.Close()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }()
+
+// _, err = db.CreateMonth(2025, time.March)
+// if err != nil {
+// 	panic(err)
+// }
+
+// err = db.CreateBillTypes([]string{"test type"})
+// if err != nil {
+// 	panic(err)
+// }
+
+// // IMPORTANT: Due dates for bills should be set according to period
+// // IMPORTANT: [monthly]: we set month to January and the day is specified by user
+// // IMPORTANT:  [yearly]: user should set month and day
+// dd, err := lib.NewDate(2025, time.February, 3)
+// if err != nil {
+// 	panic(err)
+// }
+
+// err = db.CreateNewBills([]sqlite.BillRecord{
+// 	{
+// 		TypeID:  1,
+// 		Name:    "test bill 1",
+// 		Amount:  lib.NewCurrency("32.41", lib.USD),
+// 		DueDate: dd,
+// 		Period:  sqlite.MONTHLY,
+// 	},
+// 	{
+// 		TypeID:  1,
+// 		Name:    "test bill 2",
+// 		Amount:  lib.NewCurrency("11.33", lib.USD),
+// 		DueDate: dd,
+// 		Period:  sqlite.MONTHLY,
+// 	},
+// 	{
+// 		TypeID:  1,
+// 		Name:    "test bill 3",
+// 		Amount:  lib.NewCurrency("782.23", lib.USD),
+// 		DueDate: dd,
+// 		Period:  sqlite.MONTHLY,
+// 	},
+// })
+// if err != nil {
+// 	panic(err)
+// }
+
+// err = db.CreateMonthlyBills([]sqlite.MonthlyBill{
+// 	{
+// 		BillID:   2,
+// 		IsActive: true,
+// 	},
+// 	{
+// 		BillID:   1,
+// 		IsActive: true,
+// 	},
+// 	{
+// 		BillID:   3,
+// 		IsActive: true,
+// 	},
+// })
+
+// rows, err := db.Query(sqlite.MonthlyBillsSql)
+// if err != nil {
+// 	panic(err)
+// }
+
+// var id int
+// var isActive bool
+// var amount lib.Currency
+// var dueDate lib.Date
+// var period sqlite.Period
+// var billType string
+
+// for rows.Next() {
+// 	err := rows.Scan(&id, &isActive, &amount, &dueDate, &period, &billType)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(id, isActive, amount, dueDate, period, billType)
+// }
