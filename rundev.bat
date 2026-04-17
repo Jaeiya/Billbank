d7186eb113b36394bf373b624ff639c6af39f0dd
Jaeiya <exanix@gmail.com>
2026-04-17 15:32:17 -0700

    feat(rundev): run with included cmd path
    Now we can just execute 'rundev' without any args

--- rundev.bat
+++ rundev.bat
@@ -1,6 +1,7 @@
 @echo off
 
-set PKG_PATH=github.com/jaeiya/billbank/lib/utils
+set PKG_PATH=github.com/jaeiya/billbank/internal/utils
+set CMD_PATH=.\cmd\billbank\
 
 for /f "delims=" %%a in ('git rev-parse HEAD') do set COMMIT_SHA=%%a
-go run -ldflags "-X %PKG_PATH%.commitSha=%COMMIT_SHA% -X %PKG_PATH%.codeName=alpha" %*
+go run -ldflags "-X %PKG_PATH%.commitSha=%COMMIT_SHA% -X %PKG_PATH%.codeName=alpha" %CMD_PATH%
