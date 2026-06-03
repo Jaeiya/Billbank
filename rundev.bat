8cf100faf4b639e15fd78fd9b1e1c1089b0a7814
Jaeiya <exanix@gmail.com>
2026-06-03 13:05:02 -0700

    feat(rundev): use `go run` instead of `go build`
    This effectively erases cold startup and teardown for dev builds

--- rundev.bat
+++ rundev.bat
@@ -4,4 +4,8 @@ set PKG_PATH=github.com/jaeiya/billbank/internal/utils
 set CMD_PATH=.\cmd\billbank\
 
 for /f "delims=" %%a in ('git rev-parse HEAD') do set COMMIT_SHA=%%a
-go run -ldflags "-X %PKG_PATH%.commitSha=%COMMIT_SHA% -X %PKG_PATH%.codeName=alpha" %CMD_PATH%
+go build -ldflags "-X %PKG_PATH%.commitSha=%COMMIT_SHA% -X %PKG_PATH%.codeName=alpha" -o .\billbank.exe %CMD_PATH%
+
+.\billbank.exe
+
+del billbank.exe
