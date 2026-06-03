921eb0e93fecc43e1188ed44710f2ddfa25531c4
Jaeiya <exanix@gmail.com>
2026-06-03 13:03:49 -0700

    feat(commands): add bills view WIP

--- internal/commands/bills.go
+++ internal/commands/bills.go
@@ -1,20 +1,73 @@
 package commands
 
 import (
+	"fmt"
+	"math/rand/v2"
+	"strings"
+
 	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 	"github.com/jaeiya/billbank/internal/cmdcore"
 	"github.com/jaeiya/billbank/internal/db/sqlite"
+	"github.com/jaeiya/billbank/internal/ui"
+	"github.com/jaeiya/billbank/internal/utils"
 )
+
+var tableBottom = ui.Style.BorderTop(true).
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderBackground(ui.BgColor).
+	Background(ui.BgColor)
 
 type billsModel struct {
 	cmdcore.ModelBase
-	db *sqlite.SqliteDb
+	db       *sqlite.SqliteDb
+	table    ui.TableModel
+	tableLen int
 }
 
 func NewBillsHandler(db *sqlite.SqliteDb) cmdcore.CommandHandler {
 	model := billsModel{
 		ModelBase: cmdcore.NewModelBase(),
 	}
+
+	rows := make([][]ui.TableEntry, 10)
+	for i := range rows {
+		rows[i] = createRow()
+	}
+
+	const (
+		nameLen     = 20
+		amountLen   = 11
+		dueDayLen   = 10
+		statusLen   = 10
+		intervalLen = 11
+		methodLen   = 15
+	)
+
+	model.tableLen = nameLen + amountLen + dueDayLen + statusLen + intervalLen + methodLen
+
+	var err error
+	model.table, err = ui.NewTable(
+		[]ui.TableHeader{
+			{Name: "Name", Width: nameLen},
+			{Name: "Amount", Width: amountLen},
+			{Name: "Due", Width: dueDayLen},
+			{Name: "Status", Width: statusLen},
+			{Name: "Interval", Width: intervalLen},
+			{Name: "Payment Method", Width: methodLen},
+		},
+		rows,
+		ui.WithDataAlignments([]lipgloss.Position{
+			lipgloss.Left, lipgloss.Right, lipgloss.Center, lipgloss.Left, lipgloss.Center, lipgloss.Center,
+		}),
+		ui.WithHeaderAlignments([]lipgloss.Position{
+			lipgloss.Left, lipgloss.Right, lipgloss.Center, lipgloss.Left, lipgloss.Center, lipgloss.Center,
+		}),
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	h := cmdcore.NewCmdHandler(
 		"bills",
 		[]string{"bills"},
@@ -27,10 +80,22 @@ func NewBillsHandler(db *sqlite.SqliteDb) cmdcore.CommandHandler {
 		},
 		model,
 	)
+
 	return h
 }
 
 func (m billsModel) Update(msg tea.Msg) (cmdcore.CommandModel, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		if msg.String() == "enter" {
+			idx := m.table.SelectedRow()
+			err := m.table.SetRow(idx, createRow())
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+	m.table, _ = m.table.Update(msg)
 	return m, nil
 }
 
@@ -39,5 +104,84 @@ func loadBills(m billsModel, arg *cmdcore.NoArg) (cmdcore.CommandModel, error) {
 }
 
 func viewBills(m billsModel) tea.View {
-	return tea.NewView("hello world")
+	w, h := m.ViewportSize()
+	m.table.SetWidth(w)
+	m.table.SetHeight(h)
+
+	total := "1234.56"
+	totalOffset := strings.Repeat(" ", 20+11-utils.RuneCount(total)) + total
+
+	tableTotal := ui.JoinHorizontal(
+		lipgloss.Left,
+		ui.Style.Width(m.tableLen).
+			BorderTop(true).
+			Background(ui.BgColor).
+			BorderBackground(ui.BgColor).
+			BorderStyle(lipgloss.NormalBorder()).
+			Foreground(ui.BrightGreen).
+			Render(totalOffset),
+	)
+
+	header := ui.Style.
+		PaddingTop(2).
+		PaddingBottom(3).
+		Background(ui.BgColor).
+		Width(m.tableLen).
+		Align(lipgloss.Center).
+		Render(ui.ToAsciiFont("Bills", ui.BigMoney))
+
+	return tea.NewView(
+		ui.Place(
+			w, h,
+			lipgloss.Center, lipgloss.Top,
+			ui.JoinVertical(lipgloss.Left, header, m.table.View().Content, tableTotal),
+			lipgloss.WithWhitespaceStyle(lipgloss.NewStyle().Background(ui.BgColor)),
+		),
+	)
+}
+
+func createRow() []ui.TableEntry {
+	names := [13]string{
+		"Bank of America",
+		"Netflix",
+		"Amazon",
+		"Cell Phone",
+		"Mortgage",
+		"Electric",
+		"Water",
+		"Sears",
+		"Capital One",
+		"Spectrum Internet",
+		"Spotify",
+		"Health Insurance",
+		"Groceries",
+	}
+
+	highPrice := max(rand.IntN(1500), 30)
+	lowPrice := max(30, rand.IntN(100))
+
+	price := highPrice
+	if rand.IntN(100) < 50 {
+		price = lowPrice
+	}
+	cents := rand.IntN(99)
+
+	paid := "Paid"
+	paidColor := ui.BrightGreen
+	if rand.IntN(100) < 30 {
+		paid = "Missed"
+		paidColor = ui.BrightRed
+	} else if rand.IntN(100) < 50 {
+		paid = "Pending"
+		paidColor = ui.BrightYellow
+	}
+
+	return []ui.TableEntry{
+		{Text: names[rand.IntN(len(names))]},
+		{Text: fmt.Sprintf("$%d.%02d", price, cents), Foreground: ui.Green},
+		{Text: fmt.Sprintf("%02d", rand.IntN(31)+1)},
+		{Text: paid, Foreground: paidColor},
+		{Text: "monthly", Foreground: ui.Gray},
+		{Text: "Credit Card", Foreground: ui.Gray},
+	}
 }
