7e27ddcd4a5ea74a4ce47a83e57ee926f797fd10
Jason <exanix@gmail.com>
2026-06-25 18:34:28 -0700

    fix(form): prevent trailing decimal & missing cent place in price validation

--- internal/ui/form.go
+++ internal/ui/form.go
@@ -217,6 +217,14 @@ func (fi FormInput) validate() error {
 				return errors.New("invalid price")
 			}
 
+			if strLen == 2 && inputStr[0] == '.' {
+				return errors.New("missing cents place")
+			}
+
+			if strLen == 2 && inputStr[1] == '.' {
+				return errors.New("trailing decimal not allowed")
+			}
+
 			if strLen > 2 && inputStr[strLen-3] != '.' {
 				return errors.New("too many or too few cent places")
 			}
