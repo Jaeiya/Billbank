e7d70f1b2e0a0ae3432580027cc1be099688f9fe
Jason <exanix@gmail.com>
2026-07-06 13:27:11 -0700

    feat(bills): add setup state and bill type creation

--- internal/commands/bills.go
+++ internal/commands/bills.go
@@ -1,17 +1,46 @@
 package commands
 
 import (
+	"time"
+
 	tea "charm.land/bubbletea/v2"
 	"github.com/jaeiya/billbank/internal/cmdcore"
 	"github.com/jaeiya/billbank/internal/db/sqlite"
 	"github.com/jaeiya/billbank/internal/ui"
 )
 
+type BillCmdState int
+
+const (
+	Normal BillCmdState = iota
+	Setup
+)
+
+var defaultBillTypes = [...]string{
+	"Credit Card",
+	"Donation",
+	"Education",
+	"Entertainment",
+	"Family",
+	"Food",
+	"Health",
+	"Housing",
+	"Insurance",
+	"Loan",
+	"Maintenance",
+	"Medical",
+	"Subscription",
+	"Tax",
+	"Transit",
+	"Utility",
+}
+
 type billsModel struct {
 	cmdcore.ModelBase
 	db       *sqlite.SqliteDb
 	table    ui.TableModel
 	form     ui.Form
+	state    BillCmdState
 	initForm bool
 	tableLen int
 }
@@ -19,6 +48,7 @@ type billsModel struct {
 func NewBillsHandler(db *sqlite.SqliteDb) cmdcore.CommandHandler {
 	model := billsModel{
 		ModelBase: cmdcore.NewModelBase(),
+		db:        db,
 	}
 
 	h := cmdcore.NewCmdHandler(
@@ -56,6 +86,25 @@ func (m billsModel) Update(msg tea.Msg) (cmdcore.CommandModel, tea.Cmd) {
 }
 
 func loadBills(m billsModel, arg *cmdcore.NoArg) (cmdcore.CommandModel, error) {
+	count, err := m.db.QueryBillsCount()
+	if err != nil {
+		panic(err)
+	}
+
+	if count == 0 {
+		m.state = Setup
+
+		if err = m.db.CreateBillTypes(defaultBillTypes[:]); err != nil {
+			return m, err
+		}
+
+		now := time.Now()
+		if _, err = m.db.CreateMonth(now.Year(), now.Month()); err != nil {
+			return m, err
+		}
+
+	}
+
 	return m, nil
 }
 
